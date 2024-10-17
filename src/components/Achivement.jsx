7656cd5517a077b4b8120eee42049dc809ca8f7b
arupmondol <arupmondol399@gmail.com>
2024-10-17 12:07:57 +0600

    Full ui done

---
+++ src/components/Achivement.jsx
@@ -0,0 +1,10 @@
+
+const Achivement = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Achivement
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,9 +5,9 @@ import Services from "./Services"
 
 const Form = () => {
   return (
-    <div className="text-black rounded-md  bg-slate-50 w p-6 m-6">
+    <div className="text-black rounded-md w p-6 m-6">
       <form action="">
-
+      <div className=" bg-slate-50 mb-4 ">
       <div className="grid grid-cols-2">
 
         <div className="m-4 p-4">
@@ -49,11 +49,20 @@ const Form = () => {
       </div>
 
 
-      <div className="Achivement">
+      <div className="Achivement m-4 p-4">
       <label className="text-xl" htmlFor="textarea">Add Achivements <span className="text-slate-400">(Optional)</span> </label><br/>
 
-    
+        <div className="bg-white relative mb-6 rounded-md mt-4 p-4 w-full h-16 border-solid border-2 border-slate-300 ">
+          <p className="text-slate-500">(a breif, idea, branding guideline,old desing...)</p>
+          <button className="absolute h-12 disabled: w-48 bottom-2 text-white right-0 bg-emerald-600">Add file(5mb)</button>
+        </div>
        
+      </div>
+
+      </div>
+
+      <div className=" submitButton mt-16 ">
+        <button className="bg-blue-700 h-16 w-56 font-bold text-xl text-white"> Send Inquary</button>
       </div>
         
       </form>
